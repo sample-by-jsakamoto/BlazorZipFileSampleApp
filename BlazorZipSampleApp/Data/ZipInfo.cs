118892487826e7b2688b3433f61a45ac82a318e0
nobuhiro_inagawa <lunacy171710@yahoo.co.jp>
2024-12-19 12:42:51 +0900

    first

---
+++ BlazorZipSampleApp/Data/ZipInfo.cs
@@ -0,0 +1,45 @@
+﻿using System.Text;
+
+namespace BlazorZipSampleApp.Data
+{
+    public class ZipInfo
+    {
+        public string Name { get; set; }
+
+        public List<ZipContent> Contents { get; private set; } = new List<ZipContent>();
+        public List<string> Directories { get; private set; } = new List<string>();
+        public void Append(ZipContent content)
+        {
+            Contents.Add(content);
+        }
+        public void Append(string directory)
+        {
+            Directories.Add(directory);
+        }
+
+        public string ToCsv()
+        {
+            var header = ZipContent.Header() + Environment.NewLine;
+            return header + string.Join(Environment.NewLine, Contents.Select(x => x.ToString()));
+        }
+    }
+
+    public class ZipContent
+    {
+        public byte[] Bytes { get; set; }
+        public string Name { get; set; }
+
+        public string FullName { get; set; }
+        public long Size { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Name},{Size},{FullName}";
+        }
+
+        public static string Header()
+        {
+            return $"Name,Size,FullName";
+        }
+    }
+}
---
+++ BlazorZipSampleApp/wwwroot/js/savefile.js
@@ -0,0 +1,20 @@
+﻿export const saveAsZipAsync = async (fileName, base64Str) => {
+    // convert blob
+    const byteChars = atob(base64Str);
+    const byteNumers = new Array(byteChars.length);
+    for (let i = 0; i < byteChars.length; i++) {
+        byteNumers[i] = byteChars.charCodeAt(i);
+    }
+    const byteArray = new Uint8Array(byteNumers);
+    const blob = new Blob([byteArray], { type: "application/zip" });
+
+    // create a link element with blob and click it
+    const link = document.createElement("a");
+    const url = window.URL.createObjectURL(blob);
+    link.href = url
+    link.download = fileName
+    link.click();
+
+    link.remove();
+    window.URL.revokeObjectURL(url);
+}
